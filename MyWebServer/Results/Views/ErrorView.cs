5f66e46e3445b65f9b6b707eba41a7dcf62ee29c
George <gs.ivanov@abv.bg>
2022-12-30 13:13:59 +0200

    in progress

--- MyWebServer/Results/Views/ErrorView.cs
+++ MyWebServer/Results/Views/ErrorView.cs
@@ -1,6 +1,34 @@
 ﻿namespace MyWebServer.Results.Views
 {
-    public class ErrorView
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class ErrorView : IView
     {
+        private readonly IEnumerable<string> errors;
+        private readonly string csharpCode;
+
+        public ErrorView(IEnumerable<string> errors, string csharpCode)
+        {
+            this.errors = errors;
+            this.csharpCode = csharpCode;
+        }
+
+        public string ExecuteTemplate(object viewModel, string user)
+        {
+            var html = new StringBuilder();
+
+            html.AppendLine($"View compile {this.errors.Count()} errors: </h1><ul>");
+
+            foreach (var error in errors)
+            {
+                html.AppendLine($"<li>{error}</li>");
+            }
+
+            html.AppendLine($"</ul><pre>{csharpCode}</pre>");
+
+            return html.ToString();
+        }
     }
 }
